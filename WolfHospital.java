72f1c500f715b6040994e657c68c3a423a72d1aa
ctmayn <ctmayn2@gmail.com>
2017-11-11 23:47:58 -0500

    Added the closing statements for all the things that are opened and added the create table statements

--- WolfHospital.java
+++ WolfHospital.java
@@ -10,13 +10,14 @@ public class WolfHospital{
     private static Scanner scanner = null;
 
     public static void main(String[] args){
-        try {
-        Class.forName("org.mariadb.jdbc.Driver");
-
+        
         Connection connection = null;
         Statement statement = null;
         ResultSet result = null;
 
+        try {
+        Class.forName("org.mariadb.jdbc.Driver");
+        connection = DriverManager.getConnection(jdbcURL, user, password);
         statement = connection.createStatement();
 
         scanner =  new Scanner(System.in);
@@ -28,10 +29,138 @@ public class WolfHospital{
         } catch(Throwable oops) {
             oops.printStackTrace();
         }
+        if(scanner != null){
+            scanner.close();
+        }
+        close(result);
+        close(statement);
+        close(connection);
     }
 
-    private static void setupTables(Statement statement){
+    static void close(Connection conn) {
+        if(conn != null) {
+            try { conn.close(); } catch(Throwable whatever) {}
+        }
+    }
 
+    static void close(Statement st) {
+        if(st != null) {
+            try { st.close(); } catch(Throwable whatever) {}
+        }
+    }
+
+    static void close(ResultSet rs) {
+        if(rs != null) {
+            try { rs.close(); } catch(Throwable whatever) {}
+        }
+    }
+
+    private static void setupTables(Statement statement) throws SQLException{
+        statement.executeUpdate("CREATE TABLE staff (" +
+            "id INT PRIMARY KEY, "  +
+            "name NVARCHAR2(128) NOT NULL, " +
+            "age INT NOT NULL, " +
+            "job_title NVARCHAR2(128) NOT NULL, "  +
+            "professional_title NVARCHAR2(128) NOT NULL, "  +
+            "dept NVARCHAR2(128) NOT NULL, " +
+            "contact_info NVARCHAR2(128) NOT NULL)");
+            
+        statement.executeUpdate("CREATE TABLE doctor( "  +
+            "staff_id INT PRIMARY KEY, "  +
+            "specialist NVARCHAR2(128) "  +
+            " CONSTRAINT doctors_fk FOREIGN KEY(staff_id) REFERENCES staff(id) "  +
+            "ON DELETE CASCADE)");
+            
+        statement.executeUpdate("CREATE TABLE nurse( "  +
+            "staff_id INT PRIMARY KEY, "  +
+            "CONSTRAINT nurse_fk FOREIGN KEY(staff_id) REFERENCES staff(id)  "  +
+            "ON DELETE CASCADE)");
+            
+        statement.executeUpdate("CREATE TABLE operator( "  +
+            "staff_id INT PRIMARY KEY, "  +
+            "CONSTRAINT operator_fk FOREIGN KEY(staff_id) REFERENCES staff(id) "  +
+            "ON DELETE CASCADE)");
+            
+        statement.executeUpdate("CREATE TABLE patient( "  +
+            "patient_id INT PRIMARY KEY, "  +
+            "status NVARCHAR2(128) NOT NULL, "  +
+            "gender NVARCHAR2(128) NOT NULL, "  +
+            "ssn INT, "  +
+            "name NVARCHAR2(128) NOT NULL, "  +
+            "DOB NVARCHAR2(128) NOT NULL, "  +
+            "age INT NOT NULL, "  +
+            "contact_info NVARCHAR2(128) NOT NULL)");
+            
+        statement.executeUpdate("CREATE TABLE medical_record( "  +
+            "record_num INT PRIMARY KEY, "  +
+            "res_doctor INT NOT NULL, "  +
+            "diagnosis_details NVARCHAR2(128), "  +
+            "prescription NVARCHAR2(128), "  +
+            "CONSTRAINT doctor_fk FOREIGN KEY(res_doctor) REFERENCES doctor(staff_id) "  +
+            "ON DELETE CASCADE )");
+            
+        statement.executeUpdate("CREATE TABLE billing_account( "  +
+            "billing_id INT PRIMARY KEY, "  +
+            "billing_addr NVARCHAR2(128) NOT NULL, "  +
+            "payment_info NVARCHAR2(128) NOT NULL)");
+            
+        statement.executeUpdate("CREATE TABLE ward( "  +
+            "ward_num INT PRIMARY KEY, "  +
+            "charges_per_day INT NOT NULL, "  +
+            "res_nurse INT NOT NULL, "  +
+            "capacity INT NOT NULL, "  +
+            "CONSTRAINT nurse_fk FOREIGN KEY(res_nurse) REFERENCES nurse(staff_id)  "  +
+            "ON DELETE CASCADE)");
+            
+        statement.executeUpdate("CREATE TABLE bed( "  +
+            "bed_num INT PRIMARY KEY, "  +
+            "ward_num INT PRIMARY KEY, "  +
+            "patient_id INT, "  +
+            "CONSTRAINT ward_fk FOREIGN KEY(ward_num) REFERENCES ward(ward_num)  "  +
+            "ON DELETE CASCADE "  +
+            "CONSTRAINT patient_fk FOREIGN KEY(patient_id) REFERENCES patient(patient_id)  "  +
+            "ON DELETE CASCADE)");
+            
+        statement.executeUpdate("CREATE TABLE check_in_information( "  +
+            "check_in_id INT PRIMARY KEY, "  +
+            "start_date NVARCHAR2(128) NOT NULL, "  +
+            "end_date NVARCHAR2(128), "  +
+            "bed_num INT NOT NULL, "  +
+            "ward_num INT NOT NULL, "  +
+            "CONSTRAINT bed_fk FOREIGN KEY(bed_num) REFERENCES bed(bed_num)  "  +
+            "ON DELETE CASCADE, "  +
+            "CONSTRAINT ward_fk FOREIGN KEY(ward_num) REFERENCES ward(ward_num)  "  +
+            "ON DELETE CASCADE)");
+            
+        statement.executeUpdate("CREATE TABLE test( "  +
+            "test_id INT PRIMARY KEY, "  +
+            "record_num INT NOT NULL, "  +
+            "test_name NVARCHAR2(128) NOT NULL, "  +
+            "specialist INT NOT NULL, "  +
+            "patient_id INT NOT NULL, "  +
+            "test_results NVARCHAR2(128) NOT NULL "  +
+            "CONSTRAINT specialist_fk FOREIGN KEY(specialist) REFERENCES doctor(staff_id)  "  +
+            "ON DELETE CASCADE "  +
+            "CONSTRAINT record_num_fk FOREIGN KEY(record_num) REFERENCES medical_record(record_num)  "  +
+            "ON DELETE CASCADE "  +
+            "CONSTRAINT patient_fk FOREIGN KEY(patient_id) REFERENCES patient(patient_id)  "  +
+            "ON DELETE CASCADE)");
+            
+        statement.executeUpdate("CREATE TABLE office_visit( "  +
+            "visit_num INT PRIMARY KEY "  +
+            "record_num INT, "  +
+            "billling_id INT, "  +
+            "check_in_id INT, "  +
+            "patient_id INT, "  +
+            "CONSTRAINT record_num_fk FOREIGN KEY(record_num) REFERENCES medical_record(record_num)  "  +
+            "ON DELETE CASCADE, "  +
+            "CONSTRAINT billing_fk FOREIGN KEY(billing_id) REFERENCES billing_accountf(billing_id)  "  +
+            "ON DELETE CASCADE, "  +
+            "CONSTRAINT check_in_fk FOREIGN KEY(check_in_id) REFERENCES check_in(check_in_id)  "  +
+            "ON DELETE CASCADE, "  +
+            "CONSTRAINT patient_fk FOREIGN KEY(patient_id) REFERENCES patient(patient_id)  "  +
+            "ON DELETE CASCADE)");
+            
     }
 
     private static void logOn(Statement statement){
